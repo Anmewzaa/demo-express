8937fe9e03260d1cd4aef9602579927269ed6c80
Anmewzaa <Punyakon857@gmail.com>
2025-04-11 14:36:54 +0700

    Update

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
 const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-
-const port = 8080;
+const mongoose = require("mongoose");
 
 const app = express();
+const PORT = 3000;
+
+const MONGO_URI =
+  "mongodb+srv://api_bb:cmzBNvHgcOx4Hsni@backbone-pri.8bzqn.mongodb.net/?retryWrites=true&w=majority&appName=Backbone";
 
 app.use(express.json());
-app.use(cors());
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log(`Connected to database successfully`))
+  .catch((err) => console.log(`Error : ${err}`));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ Message: "HELLO WORLD !" });
 });
 
-app.get("/api", async (req, res) => {
-  try {
-    const response = await axios.get("http://10.2.0.2:80");
-    res.send(`Nginx Response: ${response.data}`);
-  } catch (error) {
-    console.error("Error connecting to Nginx:", error);
-    res.status(500).send("Failed to connect to Nginx");
-  }
-});
-
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
